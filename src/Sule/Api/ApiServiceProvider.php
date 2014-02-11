ec21b3deb39f3f72a005231eb2728f48a06c6032
Sulaeman <me@sulaeman.com>
2014-02-11 19:54:34 +0700

    init

---
+++ src/Sule/Api/ApiServiceProvider.php
@@ -0,0 +1,63 @@
+<?php
+namespace Sule\Api;
+
+/*
+ * Author: Sulaeman <me@sulaeman.com>.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('sule/api');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->registerCommands();
+	}
+
+	/**
+     * Register the artisan commands.
+     *
+     * @return void
+     */
+    protected function registerCommands()
+    {
+        
+    }
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('api');
+	}
+
+}
