4be8d1fbebf3de7d6305f3ed2483dd1541fe0f1c
Sulaeman <me@sulaeman.com>
2014-02-18 12:33:53 +0700

    add Access-Control-Allow-Methods in config

--- src/config/config.php
+++ src/config/config.php
@@ -4,7 +4,16 @@ return array(
 
     'headers' => array(
         'Access-Control-Allow-Credentials' => 'true',
-        'Access-Control-Allow-Origin' => '*'
+        'Access-Control-Allow-Origin'      => '*',
+        'Access-Control-Allow-Methods'     => array(
+            'OPTIONS',
+            'HEAD',
+            'GET',
+            'POST',
+            'PUT',
+            'PATCH',
+            'DELETE'
+        )
     )
 
 );
