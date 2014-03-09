0562805a45e0a5e89ce8f4e9928597bfd9a875c0
Sulaeman <me@sulaeman.com>
2014-03-09 11:35:03 +0700

    add Content-MD5 to allowed headers

--- src/config/config.php
+++ src/config/config.php
@@ -6,7 +6,7 @@ return array(
         'Access-Control-Allow-Credentials' => 'true',
         'Access-Control-Allow-Origin'      => '*',
         'Access-Control-Allow-Methods'     => 'OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE',
-        'Access-Control-Allow-Headers'     => 'Origin, Accept, Content-Type, Content-Range, Content-Disposition, Authorization'
+        'Access-Control-Allow-Headers'     => 'Origin, Accept, Content-Type, Content-MD5, Content-Range, Content-Disposition, Authorization'
     )
 
 );
